f12382a454fcde3a01370960a7f7dfcbc7f1bdbd
notcamelcase <13annon@gmail.com>
2024-02-06 13:08:03 +0530

    added progressbar

--- AnimationController.py
+++ AnimationController.py
@@ -1,17 +1,23 @@
 import matplotlib.animation as animation
 import matplotlib.pyplot as plt
+from tqdm import tqdm
 
 
 class ControlledAnimation:
-    def __init__(self, figc, animate, frames=100, interval=1, video_request=False, repeat=False):
+    def __init__(self, figc, animate, frames=100, interval=1, video_request=False, repeat=False, progress_bar=False):
         self.figc = figc
         self.animate = animate
         self.frames = frames
         self.interval = interval
         self.repeat = repeat
         self.video_request = video_request
-        self.ani = animation.FuncAnimation(self.figc, self.animate, frames=self.frames, interval=self.interval,
-                                           repeat=self.repeat)
+        if not progress_bar:
+            self.ani = animation.FuncAnimation(self.figc, self.animate, frames=self.frames, interval=self.interval,
+                                               repeat=self.repeat)
+        else:
+            # noinspection PyTypeChecker
+            self.ani = animation.FuncAnimation(self.figc, self.animate, frames=tqdm(range(self.frames), initial=1, position=0), interval=self.interval,
+                                               repeat=self.repeat)
         self.m_pause = False
         self.cid = self.figc.canvas.mpl_connect('button_press_event', self.pause)
 
