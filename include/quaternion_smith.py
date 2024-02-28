1c73bdbe1adb115455f7de1a27d3ad6e8113539f
notcamelcase <13annon@gmail.com>
2024-02-28 09:34:06 +0530

    alternate kappa update

--- include/quaternion_smith.py
+++ include/quaternion_smith.py
@@ -3,6 +3,27 @@ import numpy as np
 eTOL = 1e-8
 
 
+def skew(x):
+    """
+    :param x: vector
+    :return: skew symmetric tensor for which x is axial
+    """
+    x = np.reshape(x, (3,))
+    return np.array([[0, -x[2], x[1]],
+                     [x[2], 0, -x[0]],
+                     [-x[1], x[0], 0]]
+                    )
+
+
+def axial(x):
+    """
+    x better be skew symmetric tensor
+    :param x: skew symmetric tensor
+    :return: axial vector
+    """
+    return np.array([x[2, 1], x[0, 2], x[1, 0]])
+
+
 def clamp(x, minimum, maximum):
     return np.clip(x, minimum, maximum)
 
@@ -98,6 +119,38 @@ def quat_from_scaled_rotation(x):
     return quat_from_rotation_vector(x / 2.0)
 
 
+def log(theta):
+    """
+    :param theta: total rotation
+    :return: quaterion
+    """
+    return quat_from_scaled_rotation(theta)
+
+
+def exp(q):
+    """
+    :param q: quaternion
+    :return: rotation
+    """
+    return quat_to_scaled_rotation(q)
+
+
+def kurvature_from_total_rotation(t, tds):
+    """
+    According to Crisfield & Jelenic
+    :param t: theta
+    :param tds: theta_prime
+    :return: kappa
+    """
+    norm_t = np.linalg.norm(t)
+    tensor_t = skew(t)
+    if np.isclose(norm_t, 0, atol=1e-6):
+        return tds
+    x = np.sin(norm_t) / norm_t
+    y = (1 - np.cos(norm_t)) / norm_t
+    return (1 / norm_t ** 2 * (1 - x) * t @ t.T + x * np.eye(3) - y * tensor_t) @ tds
+
+
 def quat_hermite(r0, v0, r1, v1, h_, hx_, hxx_):
     """
     :param r0: node 1 theta
@@ -107,12 +160,14 @@ def quat_hermite(r0, v0, r1, v1, h_, hx_, hxx_):
     :param h_: hermite shape fn
     :param hx_: derivative of h
     :param hxx_: double derivative of h
-    :return: interpolated quaterion and curvature
+    :return: interpolated quaterion and curvature and acceleration
     """
     qr0 = quat_from_scaled_rotation(r0)
     qr1 = quat_from_scaled_rotation(r1)
+    v0 = kurvature_from_total_rotation(r0, v0)
+    v1 = kurvature_from_total_rotation(r1, v1)
     qr1_sub_qr0 = quat_to_scaled_rotation(quat_abs(quat_mul_inv(qr1, qr0)))
     rot = quat_mul(quat_from_scaled_rotation(h_[2][0] * qr1_sub_qr0 + h_[1][0] * v0 + h_[3][0] * v1), qr0)
     vel = hx_[2][0] * qr1_sub_qr0 + hx_[1][0] * v0 + hx_[3][0] * v1
     acc = hx_[2][0] * qr1_sub_qr0 + hxx_[1][0] * v0 + hx_[3][0] * v1
-    return rot, vel
+    return rot, vel, acc
